c118ff3899c6d8bec86cc6d7047abaa7e87b810c
ych984 <lennodev@gmail.com>
2020-09-21 14:15:42 +0800

    add testing cert and testing key for SSL. Added HTTPS to backend Node.js

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,22 +1,45 @@
+import https from 'https';
+import fs from 'fs';
 import config from './config';
 import Logger from './loaders/logger';
 
+
 async function startServer() {
-    const app = await require('./app');
-    
-    app.listen(config.port, err => {
-        if (err) {        
-            Logger.error(err);
-            process.exit(1);
-            return;
-        }
-        Logger.info(`
+  const credentials = {
+    key: fs.readFileSync('./certs/server-key.pem'),
+    cert: fs.readFileSync('./certs/server-cert.pem')
+  };
+
+  const app = await require('./app');
+
+
+  //   app.listen(config.port, (err) => {
+  //     if (err) {
+  //       Logger.error(err);
+  //       process.exit(1);
+  //       return;
+  //     }
+  //     Logger.info(`
+  //         ################################################
+  //         🛡️  Server listening on port: ${config.port} 🛡️
+  //         ################################################
+  //         `);
+  //   });
+
+  const httpsServer = https.createServer(credentials, app);
+
+  httpsServer.listen(config.port, () => {
+    // if (err) {
+    //   Logger.error(err);
+    //   process.exit(1);
+    //   return;
+    // }
+    Logger.info(`
             ################################################
-            🛡️  Server listening on port: ${config.port} 🛡️ 
+            🛡️  Server listening on port: ${config.port} 🛡️
             ################################################
-            `
-        );
-    });
+            `);
+  });
 }
 
 startServer();
