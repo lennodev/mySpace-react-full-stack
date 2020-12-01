077ae6421e4d3eb08cc3f43dba956da014426193
ych984 <lennodev@gmail.com>
2020-12-01 16:27:09 +0800

    update method to read db config

--- src/server/loaders/sequelize.ts
+++ src/server/loaders/sequelize.ts
@@ -1,12 +1,28 @@
 import { Sequelize } from 'sequelize-typescript';
+import fs from 'fs';
 import Item from '../models/Item';
 import Space from '../models/Space';
 import User from '../models/User';
 import Grid from '../models/Grid';
-import SeqConfig from '../config/sequelize-config.json';
+// import SeqConfig from '/app_configs/db/sequelize-config.json';
 
-const env = process.env.NODE_ENV || 'development';
-const dbConfig = SeqConfig[env];
+let dbConfig = null;
+try {
+  let dbConnPath = '/app_configs/db/sequelize-config.json';
+  if (!fs.existsSync(dbConnPath)) {
+    // use local
+    dbConnPath = './sequelize-config.json';
+  }
+
+  const rawdata = fs.readFileSync(dbConnPath);
+  const dbConnInfo = JSON.parse(rawdata.toString());
+
+  const env = process.env.NODE_ENV || 'development';
+  dbConfig = dbConnInfo[env];
+} catch (err) {
+  console.error(err);
+}
+
 
 const sequelize:Sequelize = new Sequelize(dbConfig);
 
