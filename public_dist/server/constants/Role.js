2523aea8293ef26b8b6e643db13870906fad7221
lennodev <lennodev@gmail.com>
2020-12-05 21:40:10 +0800

    remove dist folder, add in complie tsc command in package.json

--- public_dist/server/constants/Role.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    ROLE_BASIC_USER: 'user',
-    ROLE_ADMIN_USER: 'admin',
-    ROLE_SUPPORT_USER: 'support'
-};
-//# sourceMappingURL=Role.js.map
